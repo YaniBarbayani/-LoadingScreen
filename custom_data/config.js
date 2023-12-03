378a0d2fe4f52e2e1ad1c2b6951e587e88099d97
YaniBarbayani <121999573+YaniBarbayani@users.noreply.github.com>
2023-12-03 21:34:18 +0200

    Add files via upload

---
+++ custom_data/config.js
@@ -0,0 +1,25 @@
+/*--Leaked by Mr.Shellby --- [5M LEAKS BULGARIA] --
+--Discord: https://discord.gg/ZJfn8VGjfC--- */
+
+var Config = {
+	
+	server_name: '5M LEAKS BULGARIA',
+	pause_after_intro: 1000,
+	bg_video: 'Wqsg2vWHZBM', 
+
+	radio_volume: 0.30,
+	radio_playlist: [
+        {name: 'Gangstas Paradise', link:'custom_data/song.mp3' } // patch
+    ],
+	
+	main_menu: [
+		{caption: 'Меню', onclick: LS.home.show},
+		{caption: 'Новини', onclick: LS.news.show},
+		{caption: 'Правила', onclick: LS.rules.show},
+	],
+	
+	assets :
+	{
+		bad_tv: './assets/bad_tv.mp4',
+	},
+};
---
+++ custom_data/news.js
@@ -0,0 +1,6 @@
+var News = {
+	
+	contents: [
+        {img: '', title:'Coming Soon', data:'', text:'Напишете описание / Напишете описание' }
+    ],
+};
